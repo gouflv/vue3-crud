9ff0ada8e8b6c24cd18dd5ee4628f6b49f3aea01
gouflv <lv.gouf@gmail.com>
2022-10-14 17:28:21 +0800

    feature: init useModal

--- packages/core/src/composable/index.ts
+++ packages/core/src/composable/index.ts
@@ -1,8 +1,9 @@
 export * from './useRequest'
+export * from './withInjection'
+export * from './useInjection'
+
+export * from './useModalStore'
 export * from './useListStore'
 export * from './useEditStore'
 export * from './useEditModalStore'
 export * from './useRemoveStore'
-
-export * from './withInjection'
-export * from './useInjection'
---
+++ packages/core/src/composable/useModalStore/index.ts
@@ -0,0 +1,55 @@
+import { InjectionKey, Ref, ref } from 'vue'
+import { MaybeValueFn, PlainObject } from '../../types'
+import { resolveValue } from '../../utils'
+
+export type UseModalStoreOptions<TInitialParams> = {
+  initialParams?: MaybeValueFn<TInitialParams>
+}
+
+export type UseModalStoreReturn<TParams, TInitialParams> = {
+  visible: Ref<boolean>
+  initialParams: Ref<TInitialParams>
+  params: Ref<TParams | undefined>
+  actions: {
+    open: (params?: TParams) => void
+    close: () => void
+  }
+}
+
+export const ModalStoreInjectionKey = Symbol(
+  'ModalStoreInjection'
+) as InjectionKey<UseModalStoreReturn<any, any>>
+
+export function useModalStore<
+  TParams extends PlainObject,
+  TInitialParams extends PlainObject
+>(
+  options?: UseModalStoreOptions<TInitialParams>
+): UseModalStoreReturn<TParams, TInitialParams> {
+  const initialParams = ref(
+    resolveValue(options?.initialParams) ?? {}
+  ) as Ref<TInitialParams>
+
+  const visible = ref(false)
+
+  const params = ref<TParams>()
+
+  function open(_params?: TParams) {
+    params.value = _params
+    visible.value = true
+  }
+
+  function close() {
+    visible.value = false
+  }
+
+  return {
+    visible,
+    initialParams,
+    params,
+    actions: {
+      open,
+      close
+    }
+  }
+}
